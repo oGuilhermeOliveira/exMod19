6e1379d24cadd6de5571c1db8d486e472a32b044
Guilherme Oliveira dos Santos <guilhermeolsnto@gmail.com>
2024-04-17 11:47:49 -0300

    Correção

--- src/anotacao/NomeClass.java
+++ src/anotacao/NomeClass.java
@@ -1,6 +1,7 @@
 package anotacao;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 
 /**
  * @author guiol
@@ -9,7 +10,17 @@ public class NomeClass {
 
     static void lendoAnotacao() {
         Nome nome = new Nome();
-        String annotations = nome.getNome();
-        System.out.println(annotations);
+        for (Field field : nome.getClass().getDeclaredFields()) {
+            if (field.isAnnotationPresent(Anotacao.class)) {
+                Anotacao ano = field.getAnnotation(Anotacao.class);
+                System.out.println("Nome da anotação: " + ano.toString());
+            }
+        }
+
+//        if (nome.getClass().isAnnotationPresent(Anotacao.class)) {
+//            Anotacao ano = nome.getClass().getAnnotation(Anotacao.class);
+//            System.out.println("Nome da anotação: " + ano.getClass().getName());
+//            System.out.println("valor da anotação: " + ano.value());
+//        }
     }
 }
